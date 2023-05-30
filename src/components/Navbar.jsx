0fa008ca28304b8a71465776689ae5917d306cef
szserrano <szserrano@mail.csuchico.edu>
2023-05-30 16:27:39 -0700

    Logo update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,13 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+  
   return (
     <nav 
-      className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to='/'
           className='flex items-center gap-2'
           onClick={() => {
@@ -19,7 +21,20 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px font-bold cursor-pointer'>Sydney <span className='sm:block hidden'>| Software Engineer</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
